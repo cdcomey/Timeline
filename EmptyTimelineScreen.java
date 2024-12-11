96a2d1b4df3d336249dfc08f41b08433a139bdfe
cdcomey <charlesdcomey@gmail.com>
2024-12-10 22:52:28 -0800

    began work on creating a new timeline through the GUI

--- EmptyTimelineScreen.java
+++ EmptyTimelineScreen.java
@@ -1,7 +1,8 @@
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.TextArea;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
 
 import java.awt.Graphics;
 import java.awt.Color;
@@ -13,14 +14,50 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
-public class EmptyTimelineScreen extends JPanel{
+public class EmptyTimelineScreen extends JPanel implements ActionListener{
+    private final int screenWidth = 1000;
+    private final int screenHeight = 600;
+    
+    private JTextField titleField, yearField;
+    private JTextArea categoriesTextArea, tagsTextArea;
+    private JButton createButton;
+
     public EmptyTimelineScreen(){
-        JTextField titleField = new JTextField("Title");
-        JTextArea categoriesTextArea = new JTextArea("CATEGORIES\ncategory name;R;G;B;color name");
-        JTextArea tagsTextArea = new JTextArea("TAGS (one per line)\n// comments will be ignored");
+        setLayout(null);
+
+        titleField = new JTextField("TITLE");
+        titleField.setBounds(10, screenHeight*1/20, screenWidth*1/4, 30);
+        add(titleField);
+
+        yearField = new JTextField("Starting Year");
+        yearField.setBounds(titleField.getX(), titleField.getY() + titleField.getHeight() + 10, titleField.getWidth(), titleField.getHeight());
+        add(yearField);
+
+        categoriesTextArea = new JTextArea("CATEGORIES (delete these lines)\ncategory name;R;G;B;color name");
+        categoriesTextArea.setBounds(screenWidth*1/3, titleField.getY(), titleField.getWidth(), screenHeight*2/3);
+        add(categoriesTextArea);
+
+        tagsTextArea = new JTextArea("TAGS (delete these lines)\n// comments will be ignored");
+        tagsTextArea.setBounds(screenWidth*2/3, titleField.getY(), titleField.getWidth(), categoriesTextArea.getHeight());
+        add(tagsTextArea);
+
+        createButton = new JButton("Create Timeline");
+        createButton.setBounds(screenWidth/2 - titleField.getWidth()/2, screenHeight*5/6, titleField.getWidth(), titleField.getHeight());
+        add(createButton);
+        createButton.addActionListener(this);
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+    }
+	
+	public Dimension getPreferredSize(){
+		return new Dimension(screenWidth, screenHeight);
+	}
+
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == createButton){
+            
+        }
     }
 }
--- Runner.java
+++ Runner.java
@@ -2,18 +2,25 @@ import javax.swing.JFrame;
 
 public class Runner{
     public static void main(String args[]){
-        Screen sc;
-        if (args.length > 0)
-            sc = new Screen(args[0]);
-        else
-            sc = new Screen();
+        if (args.length > 0){
+            Screen sc = new Screen(args[0]);
+            JFrame frame = new JFrame("Timeline");
             
-        JFrame frame = new JFrame("Timeline");
-         
-        frame.add(sc);
- 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+            frame.add(sc);
+    
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        } else{
+            EmptyTimelineScreen sc = new EmptyTimelineScreen();
+            JFrame frame = new JFrame("Timeline");
+            
+            frame.add(sc);
+    
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        }
+
     }
 }
