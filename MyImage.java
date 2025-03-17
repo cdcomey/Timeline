2be63223dd43ec9e9bd52b7bd6d634629d53c3f6
cdcomey <charlesdcomey@gmail.com>
2025-03-17 14:37:45 -0700

    removed URL image display

--- MyImage.java
+++ MyImage.java
@@ -3,7 +3,6 @@ import java.awt.Graphics;
 import java.awt.Color;
 import java.io.IOException;
 import java.io.File;
-import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.imageio.ImageIO;
@@ -93,22 +92,6 @@ public class MyImage{
 			g.drawImage(img, x, y, width, height, null);
         } catch (IOException e) {
 			System.err.println("IOException in MyImage.drawFromFile()\n" + e);
-		}
-	}
-	
-	public void drawFromURL(Graphics g, int x, int y, int maxWidth, int maxHeight){
-		BufferedImage img = null;
-        try {
-			URL link = new URL(imageName);
-            img = ImageIO.read(link);
-			
-			int dims[] = findDimsWithImage(img, maxWidth, maxHeight);
-			int width = dims[0];
-			int height = dims[1];
-			
-			g.drawImage(img, x - width/2, y - height/2, width, height, null);
-        } catch (IOException e) {
-			System.err.println("IOException in MyImage.drawFromURL()\n" + e);
 		}
 	}
 
